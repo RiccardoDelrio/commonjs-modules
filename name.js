97ec244741c02d8b976ba22f5fe0881a531e0119
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-03-10 15:26:37 +0100

    add fullName function to create an object with first and last name

---
+++ name.js
@@ -0,0 +1,8 @@
+const fullName = (name, surname) => {
+    const nameObj = {
+        firstName: name,
+        lastName: surname,
+    }
+    return nameObj
+}
+console.log(fullName("riccardo", "Delrio"));
